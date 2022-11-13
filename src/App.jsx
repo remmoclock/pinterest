21203d0f3dbe164eb4e0f14bebf3177447d275ff
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-11-13 14:22:25 +0100

    add uploadForm comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Title from "./Components/Title";
+import UploadForm from "./Components/UploadForm";
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <UploadForm />
     </div>
   );
 }
